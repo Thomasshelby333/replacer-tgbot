f75d0ed61e6569f5830283ac77ff9ea10c0f9a88
Thomasshelby333 <116047364+Thomasshelby333@users.noreply.github.com>
2023-05-20 12:20:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ bot.send_message(chat_id=user_id, text=message_text)
 from telegram.ext import Updater, MessageHandler, Filters
 
 # replace <BOT_TOKEN> with your bot token
-updater = Updater(token="<BOT_TOKEN>", use_context=True)
+updater = Updater(token="6a1b5084e59012093525c2443880a09a", use_context=True)
 
 def replace_word(update, context):
     # get the user input message
     message = update.message.text
 
     # replace the word "old_word" with "new_word"
-    new_message = message.replace("old_word", "new_word")
+    new_message = message.replace("link", "go")
 
     # send the new message back to the user
     update.message.reply_text(new_message)
